bdc3a5ca30b257491a80703d323866f332d47dd1
JeffryGonzalez <jeff@hypertheory.com>
2021-09-28 12:34:47 -0400

    hiring project added

---
+++ HiringApi/Controllers/HiringRequestController.cs
@@ -0,0 +1,29 @@
+﻿using HiringApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiringApi.Controllers
+{
+    public class HiringRequestController : ControllerBase
+    {
+        [HttpPost("hiring-requests")]
+        public async Task<ActionResult> AddHiringRequest([FromBody] PostHiringRequest request)
+        {
+           // Validate - if bad, return a 400.
+           if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            // Save it (for us, to a database) 
+            // return a reponse
+            //   201 Created
+            //   Location with the url of the new thing.
+            //   Send them a copy of it.
+            await Task.Delay(3000);
+            return Ok(request);
+        }
+    }
+}
